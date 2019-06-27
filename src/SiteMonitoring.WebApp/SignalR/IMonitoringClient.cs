aad40bb7c4a3365b76b4781ade41bcdbd9999931
Igor Melnikov <zero_88@bk.ru>
2019-06-27 10:52:31 +0300

    Moved SiteInfoClientData to separate file

--- src/SiteMonitoring.WebApp/SignalR/IMonitoringClient.cs
+++ src/SiteMonitoring.WebApp/SignalR/IMonitoringClient.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SiteMonitoring.WebApp.SignalR
@@ -7,21 +6,5 @@ namespace SiteMonitoring.WebApp.SignalR
     public interface IMonitoringClient
     {
         Task ReceiveMonitoringData(ICollection<SiteInfoClientData> data);
-    }
-
-    public class SiteInfoClientData
-    {
-        public SiteInfoClientData(Guid id, string uri, ulong refreshTimeInSeconds, int status)
-        {
-            Id = id;
-            Uri = uri;
-            RefreshTimeInSeconds = refreshTimeInSeconds;
-            Status = status;
-        }
-
-        public Guid Id { get; }
-        public String Uri { get; }
-        public ulong RefreshTimeInSeconds { get; }
-        public int Status { get; }
     }
 }
---
+++ src/SiteMonitoring.WebApp/SignalR/SiteInfoClientData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SiteMonitoring.WebApp.SignalR
+{
+    public class SiteInfoClientData
+    {
+        public SiteInfoClientData(Guid id, string uri, ulong refreshTimeInSeconds, int status)
+        {
+            Id = id;
+            Uri = uri;
+            RefreshTimeInSeconds = refreshTimeInSeconds;
+            Status = status;
+        }
+
+        public Guid Id { get; }
+        public string Uri { get; }
+        public ulong RefreshTimeInSeconds { get; }
+        public int Status { get; }
+    }
+}
